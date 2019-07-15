2e2494f58c3325b431d155cef67055a7244eb864
dark9924 <dark9924@naver.com>
2019-07-16 00:04:19 +0900

    ok

---
+++ src/main/java/com/rangtalk/hj/rangtalk/model/NotificationModel.java
@@ -0,0 +1,21 @@
+package com.rangtalk.hj.rangtalk.model;
+
+
+public class NotificationModel {
+
+    public String to;
+
+    public Notification notification = new Notification();
+    public Data data = new Data();
+
+    public static class Notification{
+        public String title;
+        public String text;
+    }
+
+    public static class Data{
+        public String title;
+        public String text;
+    }
+
+}
